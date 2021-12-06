97a72195ea27d6c0bb7e8e6bcb795a76c1561b6f
Mehrdad Bagheri <mehrdad.hmt@gmail.com>
2021-12-06 21:11:04 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,12 +2,14 @@ from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 from django import template
 
+from blog.models import Post
+
 
 register = template.Library()
 user_model = get_user_model()
 
 @register.filter
-def author_details(author, current_user=None):
+def author_details(author, current_user):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
@@ -27,4 +29,31 @@ def author_details(author, current_user=None):
         prefix = ""
         suffix = ""
 
-    return format_html('{}{}{}', prefix, name, suffix)  
+    return format_html('{}{}{}', prefix, name, suffix)
+  
+  
+@register.simple_tag  
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+  
+  
+@register.simple_tag  
+def endrow():
+    return format_html('</div>')
+  
+  
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+  
+  
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk).order_by('-published_at')[:5]
+    print(posts)
+    return {"title": "Recent Posts", "posts": posts}
